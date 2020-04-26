51281a61cb5d8a593de470b038d03c2d68377ce6
enricmartos <enric.martos@gmail.com>
2020-04-26 12:15:11 +0200

    build layer structure

--- web-layer/src/main/java/org/emartos/multimodulespringbootskeleton/HealthCheckGetController.java
+++ web-layer/src/main/java/org/emartos/multimodulespringbootskeleton/HealthCheckGetController.java
@@ -4,21 +4,25 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.Collections;
 import java.util.HashMap;
+import java.util.Map;
 
 @RestController
 public class HealthCheckGetController {
+    private static final String APPLICATION_NAME = "multi-module-spring-boot-skeleton";
 
     @Autowired
-    HealthCheckService healthCheckService;
+    private HealthCheckService healthCheckService;
+
 
     @GetMapping("/health-check")
-    public HashMap<String, String> index() {
-        HashMap<String, String> status = new HashMap<>();
-        status.put("application", "custom");
-        String healthCheckStatus = healthCheckService.getHealthCheckStatus();
-        status.put("status", healthCheckStatus);
-
-        return status;
+    public Map<String, String> index() {
+        return Collections.unmodifiableMap(new HashMap<String, String>() {
+            {
+                put("Application", APPLICATION_NAME);
+                put("Status", healthCheckService.getHealthCheckStatus());
+            }
+        });
     }
 }
